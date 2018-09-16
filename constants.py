85549b7267694d975d50cafa97f35d9c60512f60
Weiqi97 <weltch1997@gmail.com>
2018-09-16 09:00:48 -0400

    Use constants

---
+++ constants.py
@@ -0,0 +1,5 @@
+# Set side length of the cube.
+SIDE_LENGTH = 3
+
+# Error message when input length is wrong.
+WRONG_LENGTH = "The input object length does not match the desired length."
--- cube_shift.py
+++ cube_shift.py
@@ -1,8 +1,6 @@
 """Define what each cube face can do."""
 import numpy as np
-
-# Global constants for length of the cube.
-SIDE_LENGTH = 3
+from constants import SIDE_LENGTH, WRONG_LENGTH
 
 
 class CubeFace:
@@ -11,7 +9,7 @@ class CubeFace:
     def __init__(self, face_input: str):
         """Initialize with a string of desired length."""
         # Check length of the input.
-        assert len(face_input) == SIDE_LENGTH ^ 2, "Wrong input length."
+        assert len(face_input) == SIDE_LENGTH ^ 2, WRONG_LENGTH
 
         # Fill the cubies on a cube face.
         face_input_list = list(face_input)
@@ -36,7 +34,7 @@ class CubeFace:
 
     def fill_top_row(self, input_list: list):
         """Fill top row with a input list."""
-        assert len(input_list) == SIDE_LENGTH, "Wrong input length."
+        assert len(input_list) == SIDE_LENGTH, WRONG_LENGTH
         self.face_matrix[0] = input_list
 
     def get_bottom_row(self) -> list:
@@ -48,7 +46,7 @@ class CubeFace:
 
     def fill_bottom_row(self, input_list: list):
         """Fill bottom row with a input list."""
-        assert len(input_list) == SIDE_LENGTH, "Wrong input length."
+        assert len(input_list) == SIDE_LENGTH, WRONG_LENGTH
         self.face_matrix[2] = input_list
 
     def get_right_col(self) -> list:
@@ -60,7 +58,7 @@ class CubeFace:
 
     def fill_right_col(self, input_list: list):
         """Fill right col with a input list."""
-        assert len(input_list) == SIDE_LENGTH, "Wrong input length."
+        assert len(input_list) == SIDE_LENGTH, WRONG_LENGTH
         self.face_matrix[..., 2] = input_list
 
     def get_left_col(self) -> list:
@@ -72,7 +70,7 @@ class CubeFace:
 
     def fill_left_col(self, input_list: list):
         """Fill left col with a input list."""
-        assert len(input_list) == SIDE_LENGTH, "Wrong input length."
+        assert len(input_list) == SIDE_LENGTH, WRONG_LENGTH
         self.face_matrix[..., 0] = input_list
 
     def get_central_row(self) -> list:
@@ -84,7 +82,7 @@ class CubeFace:
 
     def fill_central_row(self, input_list: list):
         """Fill central row with a input list."""
-        assert len(input_list) == SIDE_LENGTH, "Wrong input length."
+        assert len(input_list) == SIDE_LENGTH, WRONG_LENGTH
         self.face_matrix[1] = input_list
 
     def get_central_col(self) -> list:
@@ -96,5 +94,5 @@ class CubeFace:
 
     def fill_central_col(self, input_list: list):
         """Fill central col with a input list."""
-        assert len(input_list) == SIDE_LENGTH, "Wrong input length."
+        assert len(input_list) == SIDE_LENGTH, WRONG_LENGTH
         self.face_matrix[..., 1] = input_list
