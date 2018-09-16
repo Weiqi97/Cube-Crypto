b5c52702724b1a396ee404428737b1c3337a4c68
Weiqi97 <weltch1997@gmail.com>
2018-09-16 13:21:50 -0400

    Add in constants for cube move

--- cube_encryption/constants.py
+++ cube_encryption/constants.py
@@ -1,7 +1,24 @@
 """Constants for the project."""
+from enum import Enum
 
+# ------------------------------- Constants ----------------------------------
 # Set side length of the cube.
 SIDE_LENGTH = 3
+MOVE_ANGLE = [90, 180, 270]
+CUBE_MOVE = ["right", "left", "top", "bottom", "front", "back"]
 
+
+# Set enum object for moves.
+class CubeMove(Enum):
+    right = "right"
+    left = "left"
+    top = "top"
+    bottom = "bottom"
+    front = "front"
+    back = "back"
+
+
+# ---------------------------- Error Messages --------------------------------
 # Error message when input length is wrong.
 WRONG_LENGTH = "The input object length does not match the desired length."
+WRONG_CUBE_MOVE = "The input cube move is undefined."
