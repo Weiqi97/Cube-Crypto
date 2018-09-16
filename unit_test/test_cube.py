9e62204a86c78c9624762f8ca7f948a312143930
Weiqi97 <weltch1997@gmail.com>
2018-09-16 13:51:08 -0400

    Add more tests

--- unit_test/test_cube.py
+++ unit_test/test_cube.py
@@ -13,3 +13,29 @@ class TestCubeShift:
             cube.top_face.get_right_col(),
             ["2", "2", "2"]
         )
+
+    def test_left_shift(self):
+        cube = Cube(cube_input=self.CUBE_INPUT)
+        cube.shift(move="left", angle=90)
+        np.testing.assert_array_equal(
+            cube.top_face.get_left_col(),
+            ["4", "4", "4"]
+        )
+
+    def test_top_shift(self):
+        cube = Cube(cube_input=self.CUBE_INPUT)
+        cube.shift(move="top", angle=180)
+        np.testing.assert_array_equal(
+            cube.front_face.get_top_row(),
+            ["4", "4", "4"]
+        )
+
+    def test_bottom_shift(self):
+        cube = Cube(cube_input=self.CUBE_INPUT)
+        cube.shift(move="bottom", angle=180)
+        np.testing.assert_array_equal(
+            cube.front_face.get_bottom_row(),
+            ["4", "4", "4"]
+        )
+
+
