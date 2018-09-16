f77f42c65645befd5be611249004fe25d64807ba
Weiqi97 <weltch1997@gmail.com>
2018-09-16 13:48:21 -0400

    Bug fix

--- cube_encryption/cube_face.py
+++ cube_encryption/cube_face.py
@@ -13,7 +13,7 @@ class CubeFace:
 
         # Fill the cubies on a cube face.
         face_input_list = list(face_input)
-        self.face_matrix = np.array(
+        self._face_matrix = np.array(
             [face_input_list[index: index + SIDE_LENGTH]
              for index in range(0, len(face_input_list), SIDE_LENGTH)]
         )
@@ -24,76 +24,76 @@ class CubeFace:
 
         :return: A 2D matrix with all cubies.
         """
-        return self.face_matrix
+        return self._face_matrix
 
     def get_top_row(self) -> list:
         """Get top row of the cube face.
 
         :return: A list of cubies.
         """
-        return self.face_matrix[0]
+        return self._face_matrix[0].copy()
 
     def fill_top_row(self, input_list: list):
         """Fill top row with a input list."""
         assert len(input_list) == SIDE_LENGTH, WRONG_LENGTH
-        self.face_matrix[0] = input_list
+        self._face_matrix[0] = input_list
 
     def get_bottom_row(self) -> list:
         """Get bottom row of the cube face.
 
         :return: A list of cubies.
         """
-        return self.face_matrix[2]
+        return self._face_matrix[2].copy()
 
     def fill_bottom_row(self, input_list: list):
         """Fill bottom row with a input list."""
         assert len(input_list) == SIDE_LENGTH, WRONG_LENGTH
-        self.face_matrix[2] = input_list
+        self._face_matrix[2] = input_list
 
     def get_right_col(self) -> list:
         """Get right col of the cube face.
 
         :return: A list of cubies.
         """
-        return self.face_matrix[..., 2]
+        return self._face_matrix[..., 2].copy()
 
     def fill_right_col(self, input_list: list):
         """Fill right col with a input list."""
         assert len(input_list) == SIDE_LENGTH, WRONG_LENGTH
-        self.face_matrix[..., 2] = input_list
+        self._face_matrix[..., 2] = input_list
 
     def get_left_col(self) -> list:
         """Get left col of the cube face.
 
         :return: A list of cubies.
         """
-        return self.face_matrix[..., 0]
+        return self._face_matrix[..., 0].copy()
 
     def fill_left_col(self, input_list: list):
         """Fill left col with a input list."""
         assert len(input_list) == SIDE_LENGTH, WRONG_LENGTH
-        self.face_matrix[..., 0] = input_list
+        self._face_matrix[..., 0] = input_list
 
     def get_central_row(self) -> list:
         """Get central row of the cube face.
 
         :return: A list of cubies.
         """
-        return self.face_matrix[1]
+        return self._face_matrix[1].copy()
 
     def fill_central_row(self, input_list: list):
         """Fill central row with a input list."""
         assert len(input_list) == SIDE_LENGTH, WRONG_LENGTH
-        self.face_matrix[1] = input_list
+        self._face_matrix[1] = input_list
 
     def get_central_col(self) -> list:
         """Get central col of the cube face.
 
         :return: A list of cubies.
         """
-        return self.face_matrix[..., 1]
+        return self._face_matrix[..., 1].copy()
 
     def fill_central_col(self, input_list: list):
         """Fill central col with a input list."""
         assert len(input_list) == SIDE_LENGTH, WRONG_LENGTH
-        self.face_matrix[..., 1] = input_list
+        self._face_matrix[..., 1] = input_list
