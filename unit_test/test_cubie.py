739f9f574fa1ae4e1778b8b87186ce461c119746
Weiqi97 <weltch1997@gmail.com>
2018-10-06 03:24:02 -0400

    Cubie now takes list as input

--- unit_test/test_cubie.py
+++ unit_test/test_cubie.py
@@ -4,7 +4,7 @@ from cube_encryption.constants import WRONG_CUBIE_INPUT, WRONG_ROTATION_ANGLE
 
 
 class TestCubie:
-    cubie = Cubie(cubie_input="1010")
+    cubie = Cubie(cubie_input=["1", "0", "1", "0"])
 
     def test_get_content(self):
         assert self.cubie.get_content() == deque("1010")
@@ -20,14 +20,14 @@ class TestCubie:
 class TestCubieErrorCheck:
     def test_init(self):
         try:
-            Cubie(cubie_input="abracadabra")
+            Cubie(cubie_input="abracadabra".split(""))
             raise AssertionError("Error message did not raise.")
         except AssertionError as error:
             assert str(error) == WRONG_CUBIE_INPUT
 
     def test_rotate(self):
         try:
-            Cubie(cubie_input="1010").rotate_by_angle(angle=123)
+            Cubie(cubie_input=["1", "0", "1", "0"]).rotate_by_angle(angle=123)
             raise AssertionError("Error message did not raise.")
         except AssertionError as error:
             assert str(error) == WRONG_ROTATION_ANGLE
