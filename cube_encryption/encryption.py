884b9d9658b6a265f890b29a8d208a6d8525de93
Weiqi97 <weltch1997@gmail.com>
2018-10-07 08:26:32 -0400

    Complete encryption and corresponding documentations

--- cube_encryption/encryption.py
+++ cube_encryption/encryption.py
@@ -12,7 +12,11 @@ class Encryption:
     """Perform encryption and decryption of the input."""
 
     def __init__(self, message: str, cube_side_length: int):
-        """Put the message into a cube and create a queue to hold keys."""
+        """Put the message into a cube and create a queue to hold keys.
+
+        :param message: The message to encrypt.
+        :param cube_side_length: The desired length of cube side.
+        """
         # Find the size of each block.
         block_size = cube_side_length ** 2 * 6 * CUBIE_LENGTH
         # Convert the string to binary numbers.
@@ -37,11 +41,11 @@ class Encryption:
 
     @staticmethod
     def _pad_binary_str(input_string: str, block_size: int) -> str:
-        """
+        """Pad the binary string so it can fill each cube chunk.
 
-        :param input_string:
-        :param block_size:
-        :return:
+        :param input_string: The binary string need to be padded.
+        :param block_size: Number of bits each cube can hold.
+        :return: The padded binary string.
         """
         # Find the number of block required for the encryption.
         num_block_need = int(np.ceil(len(input_string) / block_size))
@@ -56,18 +60,33 @@ class Encryption:
 
     @staticmethod
     def _un_pad_binary_str(input_string: str) -> str:
+        """Remove the pad from binary string.
+
+        :param input_string: The padded binary string.
+        :return: The un-padded binary string.
+        """
         # First strip all the trailing zeros.
         input_string = input_string.rstrip("0")
         return input_string[:-2]
 
     @staticmethod
     def string_to_binary(input_string: str) -> str:
+        """Convert Ascii string to binary string.
+
+        :param input_string: An input Ascii encoded string.
+        :return: The binary encoded equivalence of the input Ascii string.
+        """
         string_to_byte = binascii.a2b_qp(input_string)
         byte_to_binary = bin(int.from_bytes(string_to_byte, byteorder="big"))
         return byte_to_binary.replace("b", "")
 
     @staticmethod
     def binary_to_string(input_binary: str) -> str:
+        """Convert binary string to Ascii string.
+
+        :param input_binary: An input binary encoded string.
+        :return: The Ascii encoded equivalence of the input binary string.
+        """
         string_to_binary = int(input_binary, 2)
         binary_to_byte = string_to_binary.to_bytes(
             length=(string_to_binary.bit_length() + 7) // 8,
@@ -95,30 +114,29 @@ class Encryption:
 
         :param key: A list of keys used for encryption.
         """
+        # Loop through all the keys.
         for each_key in key:
-            # First rotate the cube by the key move.
-            self._cube.shift(
-                move=each_key.move, angle=each_key.angle
-            )
-            # Shift cube content.
-            self._cube.shift_cubie_content()
-            # Save the used key.
+            # Shift all the cubes.
+            for cube in self._cubes:
+                # Shift cube.
+                cube.shift(move=each_key.move, angle=each_key.angle)
+                # Shuffle bits.
+                cube.shift_cubie_content()
+            # Append the used key to key list.
             self._key.append(each_key)
 
     def decrypt(self):
         """Decrypt the message to plain text."""
         while self._key:
             # Pop the key from saved keys.
-            key = self._key.pop()
-            # Shift content backward by one space.
-            self._cube.shift_cubie_content_back()
-            # Reverse the used key.
-            self._cube.shift(
-                move=key.move, angle=(360 - key.angle)
-            )
+            each_key = self._key.pop()
+            for cube in self._cubes:
+                # Shift content backward by one space.
+                cube.shift_cubie_content_back()
+                cube.shift(move=each_key.move, angle=360 - each_key.angle)
 
     def get_pad_content(self):
-        """Print current cube state as plain text string."""
+        """Return current padded Ascii string."""
         string_list = [
             self.binary_to_string(input_binary=cube.content)
             for cube in self._cubes
@@ -126,6 +144,7 @@ class Encryption:
         return "".join(string_list)
 
     def get_un_pad_content(self):
+        """Return current un-padded Ascii string."""
         string_list = [
             self.binary_to_string(
                 input_binary=self._un_pad_binary_str(
